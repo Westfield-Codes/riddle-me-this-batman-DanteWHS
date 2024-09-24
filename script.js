0a7400f59d8eeb6f66e45f90fc18a3e0ba48b31f
DanteWHS <dante130283@student-schoolsofwestfield.org>
2024-09-24 12:11:11 -0400

    5 min

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" || answer == "pealed") alert("Correct!");
-  else alert("Wrong, Batman! Watch the video, and try again.");
+var answer = prompt("I have 4 legs in the morning, 2 legs at noon, and two legs in the evening. What am I?");
+if (answer=="man"||"Man") alert("Correct!");
+  else alert ("Incorrect!");
 }
